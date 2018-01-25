b41c8e66937bba0622848a660e0e1f7ed2a7629f
jessicacord <jessicaleecord@gmail.com>
2018-01-24 22:48:18 -0500

    initial setup with ajax running. needs styling and new theme

---
+++ app.js
@@ -0,0 +1,91 @@
+//Create preset of buttons
+var animals = ["cat", "dog", "bunny"];
+
+//Function to create buttons from array
+var createButtons = function() {
+
+    $("#buttons").html("");
+
+    animals.forEach(function(animal) {
+        
+        var newButton = $("<button>");
+        newButton.attr("data-gif", animal)
+                .addClass("gif-button")
+                .text(animal);
+        
+        $("#buttons").append(newButton);
+
+    
+    })
+}
+
+//Get value from input and store in variable
+//Create button from input and store the value
+
+$("#add-button").on("click", function(){
+
+    event.preventDefault();
+
+    var addButton = $("#new-button").val().trim();
+
+    $("#new-button").val("");
+
+    animals.push(addButton);
+    createButtons();
+
+    
+})
+
+//Create click event for each new button
+//Use value of button to create a new giphy api call
+//Return 10 gifs with the tag = to the value of the button
+
+$(document.body).on("click", ".gif-button", function(){
+    var tag = $(this).attr("data-gif");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=0UeIhgVaDyxfRXnlriyJthFl1CFq93h5&q=" + tag + "&limit=10&rating=pg";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function(response){
+        $("#gifs").html("");
+        
+        var results = response.data;
+
+        results.forEach(function(result){
+            console.log(result);
+            var rating = result.rating;
+            var gifURL = result.images.fixed_height.url;
+            var gifURLStill = result.images.fixed_height_still.url;
+            // console.log(gifURL);
+
+            var gif = $("<img>");
+            gif.attr("data-image-still", gifURLStill)
+                .attr("data-image-animate", gifURL)
+                .attr("data-state", "still")
+                .attr("src", gifURLStill)
+                .addClass("giphy");
+            
+            // var ratingText = $("<p>").text("Rating " + rating);
+
+            $("#gifs").append(gif);
+        })
+    })
+
+    
+})
+
+$(document.body).on("click", ".giphy", function() {
+
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+        $(this).attr("src", $(this).attr("data-image-animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).attr("data-image-still"));
+        $(this).attr("data-state", "still");
+    }
+})
+
+createButtons();
